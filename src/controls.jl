8bdbc3b7b15aef24a3217532cad6fb0a4eb23438
Anthony D. Blaom <anthony.blaom@gmail.com>
2021-04-01 14:41:05 +1300

    remove Save control (appears in MLJSerialization 0.2.0)

--- src/controls.jl
+++ src/controls.jl
@@ -105,42 +105,6 @@ function IterationControl.takedown(c::WithEvaluationDo, verbosity, state)
 end
 
 
-# # Save
-
-struct Save{K}
-    filename::String
-    kwargs::K
-end
-
-# constructor:
-Save(filename="machine.jlso"; kwargs...) =
-    Save(filename, kwargs)
-
-IterationControl.@create_docs(Save,
-             header="Save(filename=\"machine.jlso\"; kwargs...)",
-             example="Save(\"run3/machine.jlso\", compression=:gzip)",
-             body="Save the current state of the machine being iterated to "*
-             "disk, using the provided `filename`, decorated with a number, "*
-             "as in \"run3/machine_42.jlso\". The specified `kwargs` "*
-             "are passed to the model-specific serializer "*
-             "(JLSO for most Julia models).\n\n"*
-             "For more on what is meant by \"the machine being iterated\", "*
-             "see [`IteratedModel`](@ref).")
-
-function IterationControl.update!(c::Save,
-                                  ic_model,
-                                  verbosity,
-                                  state=(filenumber=0, ))
-    filenumber = state.filenumber + 1
-    root, suffix = splitext(c.filename)
-    filename = string(root, filenumber, suffix)
-    train_mach = IterationControl.expose(ic_model)
-    verbosity > 0 && @info "Saving \"$filename\". "
-    MLJBase.save(filename, train_mach, c.kwargs...)
-    return (filenumber=filenumber, )
-end
-
-
 # # CYCLE LEARNING RATE
 
 struct CycleLearningRate{F<:AbstractFloat}
